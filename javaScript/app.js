d36a4f92531ad754c7165a583d92cdedf06306ea
Priyanshup891 <priyanshup891@gmail.com>
2022-04-23 17:59:50 +0530

    add logic

---
+++ javaScript/app.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const block = document.querySelectorAll('.block');
+    const scoreDisplay = document.querySelector('.result');
+    const startBtn = document.querySelector('.start');
+
+    const width = 10;
+    let currentIndex = 0;
+    let appleIndex = 0;
+    let currentSnake = [2,1,0];
+    let direction = 1;
+    let score = 0;
+    let speed = 0.9;
+    let intervalTime = 0;
+    let interval = 0;
+
+    
+})
